001421628184ce6e2847c82f210eb31fba290bb8
Siia <ribki2802@gmail.com>
2023-04-30 21:59:26 +0300

    Changed code for bot

--- Bot.py
+++ Bot.py
@@ -1,3 +1,5 @@
+contacts = {}
+
 def input_error(func):
     def inner(*args):
         try:
@@ -7,57 +9,67 @@ def input_error(func):
         except ValueError:
             return "Give me name and phone please"
         except IndexError:
-            return "Not enough arguments"
+            return "This user does not exist in the database"
     return inner
 
 @input_error
-def add_contact(contact, contacts):
-    name, phone = contact.split(" ")
+def add_contact(*args):
+    name, phone = args
+
+    if name in contacts:
+        raise KeyError
+
     contacts[name] = phone
     return f"Added contact: {name}, {phone}"
 
 @input_error
-def change_phone(contact, contacts):
-    name, phone = contact.split(" ")
+def change_phone(*args):
+    name, phone = args
+
     if name not in contacts:
         raise KeyError
     contacts[name] = phone
     return f"Changed phone number for contact {name} to {phone}"
 
 @input_error
-def show_phone(contact, contacts):
-    if contact not in contacts:
-        raise KeyError
-    return f"The phone number for {contact} is {contacts[contact]}"
+def show_phone(*args):
+    name = args
+
+    if name not in contacts:
+        raise IndexError
+    return f"The phone number for {name} is {contacts[name]}"
 
 @input_error
-def show_all(contacts):
+def show_all():
     if not contacts:
         return "No contacts found"
     result = "Contacts:\n"
+
     for name, phone in contacts.items():
         result += f"{name}, {phone}\n"
+
     return result.strip()
 
 def main():
-    contacts = {}
     while True:
-        command = input("Enter command: ")
+        message = 'Unknown command'
+        command, *arguments = input("Enter command: ").split(' ')
+
         if command == "hello":
-            print("How can I help you?")
-        elif command.startswith("add "):
-            print(add_contact(command[4:], contacts))
-        elif command.startswith("change "):
-            print(change_phone(command[7:], contacts))
-        elif command.startswith("phone "):
-            print(show_phone(command[6:], contacts))
-        elif command == "show all":
-            print(show_all(contacts))
+            message = "How can I help you?"
+        elif command.startswith("add"):
+            message = add_contact(*arguments or [''])
+        elif command.startswith("change"):
+            message = change_phone(*arguments or [''])
+        elif command.startswith("phone"):
+            message = show_phone(*arguments or [''])
+        elif len(arguments) > 0 and f"{command} {arguments[0]}" == "show all":
+            message = show_all()
         elif command in ["good bye", "close", "exit", "."]:
-            print("Good bye!")
+            print('Good bye!')
             break
-        else:
-            print("Unknown command")
+
+        print(message)
 
 if __name__ == "__main__":
     main()
